39fabfb927aa110563d0e525324b61b6e1b0bf43
ssinha8752 <shubhamsinha8752@yahoo.com>
2020-06-07 13:28:13 +0530

    [Shubham] Adding files for Calculator App

---
+++ src/Buttons.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+class Buttons extends Component{
+    render(){
+    let {result} = this.props;
+    return(
+        <div className="buttons">
+        <div className="result">
+        <p>{result}</p>
+        </div>
+        <button name="1" onClick={e => this.props.onClick(e.target.name)}>1</button>
+        <button name="2" onClick={e => this.props.onClick(e.target.name)}>2</button>
+        <button name="3" onClick={e => this.props.onClick(e.target.name)}>3</button>
+        <button name="4" onClick={e => this.props.onClick(e.target.name)}>4</button>
+        <button name="5" onClick={e => this.props.onClick(e.target.name)}>5</button>
+        <button name="6" onClick={e => this.props.onClick(e.target.name)}>6</button>
+        <button name="7" onClick={e => this.props.onClick(e.target.name)}>7</button>
+        <button name="8" onClick={e => this.props.onClick(e.target.name)}>8</button>
+        <button name="9" onClick={e => this.props.onClick(e.target.name)}>9</button>
+        <button name="0" onClick={e => this.props.onClick(e.target.name)}>0</button>
+        <button name="=" onClick={e => this.props.onClick(e.target.name)}>=</button>
+        <button name="+" onClick={e => this.props.onClick(e.target.name)}>+</button>
+        <button name="-" onClick={e => this.props.onClick(e.target.name)}>-</button>
+        <button name="/" onClick={e => this.props.onClick(e.target.name)}>/</button>
+        <button name="*" onClick={e => this.props.onClick(e.target.name)}>X</button>
+        <button name="C" onClick={e => this.props.onClick(e.target.name)}>C</button>
+        </div>
+    );
+    }
+}
+export default Buttons;
